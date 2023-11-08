e3dceaad254194ed419bb17810df766af979baf6
JEremias Ryttäri <ryttari.jeremias@gmail.com>
2023-11-08 09:41:36 +0200

    Initial commit

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let projects: string[] = ["Frisbee-Frans", "Elevator Sim", "Library System", "Space Shooter", "Tank Shooter", "Train Collision Problem", "Image to Ascii converter", ]
+</script>
+
+<div class="navigation">
+    <ul>
+        <li><a href={`#/about`}>About</a></li>
+        {#each projects as project (project) }
+            <li><a href={`#/${project}`}>{project}</a></li>
+        {/each}
+    </ul>
+</div>
