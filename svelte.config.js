8ca78260c8a70872113d077b6d0cb0f7e631e265
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2023-11-13 12:47:38 +0200

    please

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+			pages: 'build'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
